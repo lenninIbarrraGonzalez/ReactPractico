f0866df83d3ae44605dce8051654c0117da6d4ed
Lennin666 <soporte@allnexus.com.co>
2020-06-04 15:49:45 -0500

    Estructura del carousel y carousel item

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
-import '../assets/styles/App.scss';
+import '../assets/styles/components/App.scss';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
 
 const App = () => (
   <div className="App">
     <Header />
     <Search />
+
+    <Categories>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
   </div>
 );
 
