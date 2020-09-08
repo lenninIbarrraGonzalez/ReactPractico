c6cae4eaa85b64e660d939ba831017bfa7dc9fca
Lennin666 <soporte@allnexus.com.co>
2020-09-07 21:20:51 -0500

    ajustes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import '../assets/styles/components/Footer.scss';
 
 const Footer = () => (
-
   <footer className='footer'>
     <a href='/'>Terminos de uso</a>
     <a href='/'>Declaración de privacidad</a>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import '../assets/styles/App.scss';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import "../assets/styles/App.scss";
 import useInitialState from '../hooks/useInitialState';
 
 const API = 'http://localhost:3000/initialState';
